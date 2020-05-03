4af676d12f5ef90ab2710908214e293a651e21cf
正义碳 <1633100573@qq.com>
2020-05-02 22:57:30 -0400

    44444

--- proj1a/NodeTest.java
+++ proj1a/NodeTest.java
@@ -521,7 +521,7 @@ public class NodeTest {
 
         assertEquals(8, ArrayDeque.removeFirst());
     }
-    
+
     @Test
     public void getADTest20() {
         ArrayDeque ArrayDeque = new ArrayDeque();
@@ -542,24 +542,4 @@ public class NodeTest {
         assertEquals(1, ArrayDeque.removeFirst());
     }
 
-    @Test
-    public void getADTest21() {
-        ArrayDeque ArrayDeque = new ArrayDeque();
-
-        ArrayDeque.addLast(0);
-        ArrayDeque.addLast(1);
-        ArrayDeque.removeFirst();
-        ArrayDeque.addLast(3);
-        ArrayDeque.addLast(4);
-        ArrayDeque.addLast(5);
-        ArrayDeque.addLast(6);
-        ArrayDeque.addLast(7);
-        ArrayDeque.addLast(8);
-        ArrayDeque.addLast(9);
-        ArrayDeque.addLast(10);
-//        ArrayDeque.removeFirst();
-
-        assertEquals(1, ArrayDeque.removeFirst());
-    }
 }
-
