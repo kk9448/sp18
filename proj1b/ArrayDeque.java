e752bb7d533efc1c3c65acb10bdafe2eba5a55b9
正义碳 <1633100573@qq.com>
2020-05-08 20:23:22 -0400

    proj1b

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,169 @@
+
+public class ArrayDeque<T> implements Deque<T>{
+    private T[] item = (T[]) new Object[8];
+    private int tail = 0;
+    private int head = item.length - 1;
+    private int sizeHead = 0;
+    private int sizeTail = 0;
+    private int size = 0;
+    private int firstEnd = item.length - 1;
+    private int tailFirst = 0;
+
+
+    public void addFirst(T x) {
+        if (size == item.length || head == tail || tail == head) {
+            resize();
+        }
+        item[head] = x;
+        if (head != 0) {
+            head = head - 1;
+            sizeHead = sizeHead + 1;
+            size = sizeHead + sizeTail;
+        } else {
+            resize();
+        }
+
+    }
+
+    public void addLast(T x) {
+        if (size == item.length || head == tail || tail == head) {
+//            sizeTail = sizeTail +1;
+            resize();
+        }
+        item[tail] = x;
+        if (tail != item.length - 1) {
+            tail = tail + 1;
+            sizeTail = sizeTail + 1;
+            size = sizeHead + sizeTail;
+        } else {
+//            sizeTail = sizeTail +1;
+            resize();
+        }
+
+    }
+
+    public T removeLast() {
+        if (size < item.length / 4) {
+            contractedSize();
+        }
+        if (sizeTail == 0) {
+            if (sizeHead == 0) {
+                return null;
+            }
+            sizeHead = sizeHead - 1;
+            size = sizeHead + sizeTail;
+            T a = removeFirstEnd();
+            return a;
+        }
+        int tmp = tail - 1;
+//        if (tail == 1) {
+//            tail = 0;
+//        } else {
+//            tail = tail - 2;
+//        }
+        tail = tail - 1;
+        sizeTail = sizeTail - 1;
+        size = sizeHead + sizeTail;
+        return item[tmp];
+    }
+
+    private T removeFirstEnd() {
+        int tmp = firstEnd;
+        firstEnd = firstEnd - 1;
+        return item[tmp];
+
+    }
+
+    public T removeFirst() {
+        if (size < item.length / 4) {
+            contractedSize();
+        }
+        if (sizeHead == 0) {
+            if (sizeTail == 0) {
+                return null;
+            }
+            sizeTail = sizeTail - 1;
+            size = sizeHead + sizeTail;
+            T a = removeTailFirst();
+            return a;
+        }
+        int tmp = head + 1;
+        head = head + 1;
+        sizeHead = sizeHead - 1;
+        size = sizeHead + sizeTail;
+        return item[tmp];
+    }
+
+    private T removeTailFirst() {
+        int tmp = tailFirst;
+        tailFirst = tailFirst + 1;
+        return item[tmp];
+
+    }
+
+    public boolean isEmpty() {
+//        if (size == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        if (sizeHead > 0) {
+            for (int i = head + 1; i <= item.length - 1; i++, head++) {
+                System.out.print(item[i] + " ");
+            }
+        }
+        if (sizeTail > 0) {
+            for (int i = 0; i <= tail - 1; i++) {
+                System.out.print(item[i] + " ");
+            }
+        }
+    }
+
+    public T get(int index) {
+        if (index < 0) {
+            return null;
+        }
+        if (index >= sizeHead) {
+            int remain = index - sizeHead;
+            if (remain > sizeTail) {
+                return null;
+            } else {
+                return item[remain + tailFirst];
+            }
+        } else {
+            return item[head + 1 + index];
+        }
+
+    }
+
+    private void resize() {
+        T[] item2 = (T[]) new Object[item.length * 2];
+        System.arraycopy(item, tailFirst, item2, 0, sizeTail);
+        System.arraycopy(item, head, item2, item2.length - sizeHead - 1, sizeHead + 1);
+        item = item2;
+        head = item2.length - sizeHead - 1;
+        firstEnd = item.length - 1;
+        tail = tail - tailFirst;
+        tailFirst = 0;
+    }
+
+    private void contractedSize() {
+        T[] item2 = (T[]) new Object[item.length / 2];
+        System.arraycopy(item, tailFirst, item2, 0, sizeTail);
+        System.arraycopy(item, head, item2, item2.length - sizeHead - 1, sizeHead + 1);
+        item = item2;
+        head = item2.length - sizeHead - 1;
+        firstEnd = item.length - 1;
+        tail = tail - tailFirst;
+        tailFirst = 0;
+    }
+
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,170 @@
+public class LinkedListDeque<T> implements Deque<T>{
+
+    private Node sentinel;
+
+    private class Node {
+        private T item;
+        private int size;
+        Node lastPointer;
+        private Node prev;
+        private Node rest;
+
+        public Node() {
+//            Type;
+            size = 0;
+            rest = null;
+        }
+        public Node(T x) {
+            item = x;
+            size = 1;
+            rest = null;
+        }
+    }
+
+
+
+    public LinkedListDeque() {
+        sentinel = new Node();
+    }
+
+//    public LinkedListDeque(T x) {
+//        sentinel = new Node();
+//        Node B = new Node(x);
+//        sentinel.rest = B;
+//        sentinel.size = sentinel.size + 1;
+//        sentinel.LastPointer = B;
+//    }
+
+    public void printDeque() {
+        if (sentinel.rest == null) {
+            return;
+        }
+        Node A = sentinel.rest;
+        for (; A.rest != null; A = A.rest) {
+            System.out.print(A.item + " ");
+        }
+        System.out.print(A.item + " ");
+    }
+
+    public void addFirst(T x) {
+        Node A = new Node(x);
+        if (sentinel.size == 0) {
+            sentinel.lastPointer = A;
+            sentinel.rest = A;
+        } else {
+            A.rest = sentinel.rest;
+            sentinel.rest.prev = A;
+            sentinel.rest = A;
+
+
+        }
+        A.prev = sentinel;
+        sentinel.size = sentinel.size + 1;
+
+
+    }
+    public void addLast(T x) {
+
+
+        Node A = new Node(x);
+        if (sentinel.lastPointer == null) {
+            sentinel.rest = A;
+            sentinel.lastPointer = A;
+            A.prev = sentinel;
+        } else {
+            sentinel.lastPointer.rest = A;
+            A.prev = sentinel.lastPointer;
+            sentinel.lastPointer = A;
+
+        }
+
+        sentinel.size = sentinel.size + 1;
+
+    }
+
+    public boolean isEmpty() {
+        if (sentinel.size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return sentinel.size;
+    }
+
+    public T removeFirst() {
+        if (sentinel.rest == null) {
+            return null;
+        }
+        T tmp = sentinel.rest.item;
+        if (sentinel.rest.rest == null) {
+            sentinel.lastPointer.rest = null;
+            sentinel.lastPointer = null;
+            sentinel.rest = null;
+            if (sentinel.size != 0) {
+                sentinel.size = sentinel.size - 1;
+            }
+            return tmp;
+        }
+        sentinel.rest.rest.prev = sentinel;
+        sentinel.rest = sentinel.rest.rest;
+//        sentinel.LastPointer = sentinel.LastPointer.prev;
+//        if (sentinel.size != 0) {
+////            sentinel.size = sentinel.size - 1;
+//        }
+        sentinel.size = sentinel.size - 1;
+        return tmp;
+    }
+
+    public T removeLast() {
+        if (sentinel.rest == null) {
+            return null;
+        }
+        T A = sentinel.lastPointer.item;
+        sentinel.lastPointer = sentinel.lastPointer.prev;
+        sentinel.lastPointer.rest = null;
+        if (sentinel.size != 0) {
+            sentinel.size = sentinel.size - 1;
+        }
+
+        return A;
+
+    }
+    public T get(int index) {
+        if (index < 0) {
+            return null;
+        }
+        Node A = sentinel.rest;
+        for (int i = 0; i < index; A = A.rest) {
+            i++;
+        }
+        return A.item;
+    }
+
+    public T getRecursive(int index) {
+        if (index < 0) {
+            return null;
+        }
+        Node A = sentinel.rest;
+        T B = recursive(0, index, A);
+        return B;
+
+
+    }
+
+    private T recursive(int x, int index, Node A) {
+        if (x == index) {
+            return A.item;
+        }
+        A = A.rest;
+        T B;
+        B = recursive(x + 1, index, A);
+        return B;
+    }
+
+
+
+
+
+}
