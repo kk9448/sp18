7882f4121d024cdecaa3f41843f1c48be4d6438e
kk9448 <1633100573@qq.com>
2020-06-03 16:24:04 -0400

    hw2 phase 1

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,31 +3,55 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int size;
+    boolean [] status;
+    int openedSite;
+    WeightedQuickUnionUF x;
     // create N-by-N grid, with all sites initially blocked
-    public Percolation(int N) {}
+    public Percolation(int N) {
+        openedSite = 0;
+        size = N;
+        status = new boolean[N * N];
+        x = new WeightedQuickUnionUF(N);
+    }
 
     // open the site (row, col) if it is not open already
     public void open(int row, int col) {
         if (row < 0 || col < 0) {
             throw new IndexOutOfBoundsException();
         }
+        if (!isOpen(row, col)) {
+            status[convertToSingle(row, col)] = true;
+            openedSite++;
+        }
     }
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return false;
+        if (row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return status[convertToSingle(row, col)];
     }
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException();
+        }
         return false;
     }
     // number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return openedSite;
     }
 
     // does the system percolate?
     public boolean percolates() {
         return false;
+    }
+    // a = row , b = column
+    private int convertToSingle(int a, int b) {
+        int number = a * size + b;
+        return number;
     }
 
     // use for unit testing (not required)
