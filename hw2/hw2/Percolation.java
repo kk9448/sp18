9e78b848eb08ef10b3f7d39e9566c593dcb82f3b
kk9448 <1633100573@qq.com>
2020-06-03 13:29:33 -0400

    hw 2 begin

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,38 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {}
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (row < 0 || col < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+
+    // use for unit testing (not required)
+    public static void main(String[] args) {
+
+    }
+
+
 
 }
