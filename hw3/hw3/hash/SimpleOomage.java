c74cb5a6094606702c75c2fbbc48b4948ca9e0b8
kk9448 <1633100573@qq.com>
2020-06-12 23:32:54 -0400

    fix hashCode()

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -39,7 +39,7 @@ public class SimpleOomage implements Oomage {
             return red + green + blue;
         } else {
             // DONE: Write a perfect hash function for Simple Oomages.
-            return red / 5 + blue / 5 + red / 5;
+            return red / 5 * 31 + blue / 5 * 31 * 31 + green / 5 * 31 * 31 * 31;
         }
     }
 
