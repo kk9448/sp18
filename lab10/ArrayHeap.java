d24ce43d29dbe39cc035979e9c02252dcc4a5411
kk9448 <1633100573@qq.com>
2020-06-16 19:43:46 -0400

    fix

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -105,7 +105,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private void swim(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
-//        validateSinkSwimArg(index);
+        validateSinkSwimArg(index);
         /** DONE: Your code here. */
         int tmp = index;
         if (tmp == 1) {
@@ -131,7 +131,7 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     private void sink(int index) {
         // Throws an exception if index is invalid. DON'T CHANGE THIS LINE.
-//        validateSinkSwimArg(index);
+        validateSinkSwimArg(index);
 
         /** DONE: Your code here. */
         int tmp = index;
@@ -237,7 +237,9 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
         swap(1, size);
         T min = contents[size].item();
         size -= 1;
-        sink(1);
+        if (size > 0) {
+            sink(1);
+        }
         return min;
     }
 
