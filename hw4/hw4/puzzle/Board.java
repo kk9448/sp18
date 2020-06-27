02822cf27b6934e6cac81b8966605427519f5b9b
kk9448 <1633100573@qq.com>
2020-06-27 14:35:45 -0400

    task 1

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -4,18 +4,18 @@ public class Board {
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        int N = size();
-        s.append(N + "\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
-            }
-            s.append("\n");
-        }
-        s.append("\n");
-        return s.toString();
-    }
+//    public String toString() {
+//        StringBuilder s = new StringBuilder();
+//        int N = size();
+//        s.append(N + "\n");
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                s.append(String.format("%2d ", tileAt(i,j)));
+//            }
+//            s.append("\n");
+//        }
+//        s.append("\n");
+//        return s.toString();
+//    }
 
 }
--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -2,18 +2,79 @@ package hw4.puzzle;
 
 import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.*;
+
 public class Solver {
-    public Solver(WorldState initial){
-        MinPQ<WorldState> pq = new MinPQ<>();
-        for (WorldState x : initial.neighbors()) {
-            pq.insert(x);
+    private Queue<WorldState> q;
+
+    private class Node extends Word{
+        private int totalValue = 0;
+        private int stepNum = 0;
+        private Node pre = null;
+        private boolean mark = false;
+        /**
+         * Creates a new Word.
+         *
+         * @param w
+         * @param g
+         */
+        public Node(String w, String g) {
+            super(w, g);
+        }
+        public void calTotal(){
+            totalValue =  stepNum + estimatedDistanceToGoal();
         }
 
     }
-    public int moves(){
-        return 0;
+
+    public Solver(WorldState initial){
+        if (initial == null) {
+            return;
+        }
+        if (initial.estimatedDistanceToGoal() == 0) {
+            q.add(initial);
+            return;
+        }
+        Node aNode = (Node) initial;
+        MinPQ<Node> pq = new MinPQ<>(new CompareNode());
+        q = new LinkedList<>();
+        for (WorldState x : initial.neighbors()) {
+            Node nodeX = (Node) x;
+            if (nodeX.mark == false) {
+                nodeX.stepNum++;
+                nodeX.calTotal();
+                nodeX.pre = aNode;
+                nodeX.mark = true;
+                pq.insert(nodeX);
+            }
+        }
+        Node result = pq.delMin();
+        while (result.estimatedDistanceToGoal() != 0) {
+            if (pq.isEmpty()) {
+                q = null;
+                break;
+            }
+            result = pq.delMin();
+            for (WorldState x : result.neighbors()) {
+                Node nodeX = (Node) x;
+                pq.insert(nodeX);
+            }
+        }
+
     }
-    public Iterable<WorldState> solution(){
-     return null;
+
+    class CompareNode implements Comparator<Node> {
+        @Override
+        public int compare(Node t0, Node t1) {
+            return t0.totalValue - t1.totalValue;
+        }
+    }
+
+
+    public int moves(){
+        return stepNum;
+    }
+    public Iterable<WorldState> solution() {
+        return q;
     }
 }
