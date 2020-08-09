4bea39f20e074929aea4875759dea345904120c4
kk9448 <1633100573@qq.com>
2020-08-09 15:26:43 -0400

    others test

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -1,39 +1,28 @@
 import edu.princeton.cs.algs4.Queue;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class MergeSort {
+
     /**
-     * Removes and returns the smallest item that is in q1 or q2.
-     *
-     * The method assumes that both q1 and q2 are in sorted order,
-     * with the smallest item first. At
+     * Removes and returns the smallest item that is in q1 or q2. <p>
+     * The method assumes that both q1 and q2 are in sorted order, with the smallest item first. At
      * most one of q1 or q2 can be empty (but both cannot be empty).
      *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      The smallest item that is in q1 or q2.
-     * @param   <Item> elements in the queue.
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return The smallest item that is in q1 or q2.
      */
-    private static <Item extends Comparable> Item getMin(
-            Queue<Item> q1, Queue<Item> q2) {
+    private static <Item extends Comparable> Item getMin(Queue<Item> q1, Queue<Item> q2) {
         if (q1.isEmpty()) {
             return q2.dequeue();
         } else if (q2.isEmpty()) {
             return q1.dequeue();
         } else {
-            /**
-             * Peek at the minimum item in each queue (which will be at the front, since the
-             * queues are sorted) to determine which is smaller.
-             */
+            // Peek at the minimum item in each queue (which will be at the front, since the
+            // queues are sorted) to determine which is smaller.
             Comparable q1Min = q1.peek();
             Comparable q2Min = q2.peek();
             if (q1Min.compareTo(q2Min) <= 0) {
-                /**
-                 * Make sure to call dequeue, so that
-                 * the minimum item gets removed.
-                 */
+                // Make sure to call dequeue, so that the minimum item gets removed.
                 return q1.dequeue();
             } else {
                 return q2.dequeue();
@@ -43,106 +32,78 @@ public class MergeSort {
 
     /**
      * Returns a queue of queues that each contain one item from items.
-     * @param   <Item> elements in the queue.
-     * @param   items  input Queue.
-    /**
-     * 返回类型为 Queue<Queue<Item>> 前面的<>括号, 声明Item继承了Comparable
      */
-
-
-    private static <Item extends Comparable> Queue<Queue<Item>> makeSingleItemQueues(Queue<Item> items) {
-        /**
-         * DONE: Your code here!
-         */
-        Queue<Queue<Item>> res = new Queue<>();
-        for (Item x : items) {
-            Queue<Item> q = new Queue<>();
-            q.enqueue(x);
-            res.enqueue(q);
+    private static <Item extends Comparable> Queue<Queue<Item>> makeSingleItemQueues(
+            Queue<Item> items) {
+        Queue<Queue<Item>> queues = new Queue<>();
+        for (Item item : items) {
+            Queue<Item> queue = new Queue<>(); // Create single queue
+            queue.enqueue(item);
+            queues.enqueue(queue); // Enqueue the single queue into the queues to be returned
         }
-        return res;
+        return queues;
     }
 
     /**
-     * Returns a new queue that contains the items in q1 and q2
-     * in sorted order.
+     * Returns a new queue that contains the items in q1 and q2 in sorted order.
+     * <p>
+     * This method should take time linear in the total number of items in q1 and q2.  After
+     * running this method, q1 and q2 will be empty, and all of their items will be in the
+     * returned queue.
      *
-     * This method should take time linear in the total
-     * number of items in q1 and q2. After running this
-     * method, q1 and q2 will be empty, and all of their
-     * items will be in the returned queue.
-     *
-     * @param   q1  A Queue in sorted order from least to greatest.
-     * @param   q2  A Queue in sorted order from least to greatest.
-     * @return      A Queue containing all of the q1 and q2 in sorted order, from least to
-     *              greatest.
-     * @param   <Item> elements in the queue
-     *
+     * @param q1 A Queue in sorted order from least to greatest.
+     * @param q2 A Queue in sorted order from least to greatest.
+     * @return A Queue containing all of the q1 and q2 in sorted order, from least to
+     * greatest.
      */
-
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
-        /**
-         * DONE: Your code here!
-         */
-        Queue res = new Queue();
-        while (!q1.isEmpty() || !q2.isEmpty()) {
-            if (q1.isEmpty()) {
-                res.enqueue(q2.dequeue());
-            } else if (q2.isEmpty()) {
-                res.enqueue(q1.dequeue());
-            } else {
-                res.enqueue(getMin(q1, q2));
-            }
+        Queue<Item> merged = new Queue<>();
+        while (!(q1.isEmpty() && q2.isEmpty())) {
+            merged.enqueue(getMin(q1, q2));
         }
-        return res;
+        return merged;
     }
 
-    /** Returns a Queue that contains the given items
-     *  @param <Item> elements in the queue
-     *  @param items input Queue
-     * sorted from least to greatest. */
-    public static <Item extends Comparable>
-        Queue<Item> mergeSort(Queue<Item> items) {
-        /**
-         * DONE: Your code here!
-         */
+    /**
+     * Returns a Queue that contains the given items sorted from least to greatest.
+     */
+    public static <Item extends Comparable> Queue<Item> mergeSort(
+            Queue<Item> items) {
         if (items.size() <= 1) {
             return items;
         }
-        Queue<Queue<Item>> singleQueue = makeSingleItemQueues(items);
-        Queue leftQueue = new Queue();
-        Queue rightQueue = new Queue();
-        int sizeLeft = items.size() / 2;
-        for (int i = 0; i < sizeLeft; i++) {
-            leftQueue.enqueue(items.dequeue());
+        Queue<Queue<Item>> queues = makeSingleItemQueues(items);
+        Queue<Item> qLeft = new Queue<>();
+        Queue<Item> qRight = new Queue<>();
+        int leftSize = queues.size() / 2; // Make left queue
+        for (int i = 0; i < leftSize; i += 1) {
+            qLeft.enqueue(queues.dequeue().dequeue());
         }
-        int sizeRight = items.size();
-        for (int i = 0; i < sizeRight; i++) {
-            leftQueue.enqueue(items.dequeue());
+        int rightSize = queues.size(); // Make right queue
+        for (int i = 0; i < rightSize; i += 1) {
+            qRight.enqueue(queues.dequeue().dequeue());
         }
-        Queue<Item> left = mergeSort(leftQueue);
-        Queue<Item> right = mergeSort(rightQueue);
-        return mergeSortedQueues(left, right);
+        Queue<Item> q1 = mergeSort(qLeft);
+        Queue<Item> q2 = mergeSort(qRight);
+        return mergeSortedQueues(q1, q2);
     }
-    /**
-     * test mergeSort
-     * */
-    @Test
-    public void test1() {
-        Queue<String> students = new Queue<String>();
-        Queue<String> rightAns = new Queue<String>();
-        students.enqueue("Alice");
-        students.enqueue("Vanessa");
-        students.enqueue("Ethan");
-        rightAns.enqueue("Alice");
-        rightAns.enqueue("Ethan");
-        rightAns.enqueue("Vanessa");
-        Queue<String> res = mergeSort(students);
-        for (int i = 0; i < students.size(); i++) {
-            assertEquals(res.dequeue(), rightAns.dequeue());
-        }
-        assertTrue(true);
 
+    public static void main(String[] args) {
+        Queue<String> languages = new Queue<>();
+        languages.enqueue("Python"); // Add my language-learning history
+        languages.enqueue("SQL");
+        languages.enqueue("Java");
+        languages.enqueue("Julia");
+        languages.enqueue("JavaScripts");
+        languages.enqueue("Lisp??");
+        languages.enqueue("Lisp??"); // Checks duplicated
+        Queue<String> sortedLanguages = MergeSort.mergeSort(languages);
+
+        // Should print `Python SQL Java Julia JavaScripts Lisp?? Lisp??`
+        System.out.println(languages.toString());
+        // Should print `Java JavaScript Julia Lisp?? Lisp?? Python SQL`
+        System.out.println(sortedLanguages.toString());
     }
+
 }
